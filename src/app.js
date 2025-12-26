a91b9b1d62a07bec7a4915b6af5bcf634402deac
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-12-26 11:20:30 +0300

    create the post route and controller

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const app=express()
 app.use(express.json())
 
 import userRouter from '../src/routes/user.route.js'
+import postRouter from '../src/routes/post.route.js'
 
 app.use('/api/v1/users',userRouter)
+app.use('/api/v1/posts',postRouter)
 
 
 
