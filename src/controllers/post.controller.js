3a7dd3f969560d9b5942cd6bd9aa8543d448ff23
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-12-26 11:49:26 +0300

    make two other crud route

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -27,4 +27,60 @@ const createPost= async (req,res)=>{
     }
 }
 
-export{createPost}
+const getposts= async(req,res)=>{
+    try {
+
+        const getposts=await Post.find();
+        res.status(200).json(post)
+        
+    } catch (error) {
+            res.status(500).json({
+            message:'internal server error'
+        })
+    }
+}
+
+
+const updatePost=async (req,res)=>{
+    try {
+
+        if(Object.keys(req.body).length===0){
+            return res.status(400).json({
+                message:"no data provided for update"
+            })
+
+        }
+        const post = await Post.findByIdAndUpdate(req.params.id,req.body, {new:true});
+
+        if(!post) return res.status(404).json({
+            message:"post not found"
+        });
+        res.status(200).json({
+            message:"post updated successfully",post
+        })
+        
+    } catch (error) {
+          res.status(500).json({
+            message:'internal server error'
+        }) 
+    }
+}
+
+
+const deletePost=async(req,res)=>{
+    try {
+
+        const deleted= await Post.findByIdAndDelete(req.params.id);
+        if(!deleted)return res.status(404).json({
+            message:"post not found"
+        })
+        res.status(200).json({
+            message:"post successfully deleted"
+        })
+    } catch (error) {
+           res.status(500).json({
+            message:'internal server error'
+        }) 
+    }
+}
+export{createPost ,getposts,updatePost,deletePost}
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost, getposts, updatePost } from "../controllers/post.controller.js";
 
 const router=Router();
 
 
 router.route('/create').post(createPost)
+router.route('/getPosts').post(getposts)
+router.route('/update/:id ').patch(updatePost)
+router.route('/delete/:id ').delete(deletePost)
 
 export{router}
